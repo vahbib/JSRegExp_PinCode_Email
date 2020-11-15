e049a47ccb56f16f6ab909cbc11ebb38f5a01f5f
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-15 11:17:33 +0530

    PIN Code is a 6 digit code

--- pinCode.js
+++ pinCode.js
@@ -1,1 +1,10 @@
 console.log("Welcome to PinCode RegExp");
+var prompt = require('prompt-sync')();
+var pin = prompt('Enter PinCode Here: ');
+// UC1
+var pinCodeRegEx =new RegExp("^[0-9]{6}$");
+if(pinCodeRegEx.test(pin)) {
+    console.log("Pin Code: "+pin+" is Valid.");
+} else {
+    console.log("Pin Code: "+pin+" is Invalid.");
+}
