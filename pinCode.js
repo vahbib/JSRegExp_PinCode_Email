d73cf16661d36515c9dceee22c9d5098c2a2f231
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-15 11:29:17 +0530

    Restrict the PIN code from taking alphabets or special characters at the beginning.

--- pinCode.js
+++ pinCode.js
@@ -2,9 +2,16 @@ console.log("Welcome to PinCode RegExp");
 var prompt = require('prompt-sync')();
 var pin = prompt('Enter PinCode Here: ');
 // UC1
-var pinCodeRegEx =new RegExp("^[0-9]{6}$");
-if(pinCodeRegEx.test(pin)) {
-    console.log("Pin Code: "+pin+" is Valid.");
+var pinCodeRegExUc1 =new RegExp("^[0-9]{6}$");
+if(pinCodeRegExUc1.test(pin)) {
+    console.log("UC1: Pin Code: "+pin+" is Valid.");
 } else {
-    console.log("Pin Code: "+pin+" is Invalid.");
+    console.log("UC1: Pin Code: "+pin+" is Invalid.");
+}
+// UC2
+var pinCodeRegExUc2 =new RegExp("^[^a-zA-z][0-9]{6}$");
+if(pinCodeRegExUc2.test(pin)) {
+    console.log("UC2: Pin Code: "+pin+" is Valid.");
+} else {
+    console.log("UC2: Pin Code: "+pin+" is Invalid.");
 }
