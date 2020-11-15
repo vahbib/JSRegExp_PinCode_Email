ded0d827fbf4a23d7f70d07e246bbd0fb226e9de
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-15 11:28:02 +0530

    Ensure @ and validate the mandatory 2nd part

--- email.js
+++ email.js
@@ -2,9 +2,16 @@ console.log("Welcome to Email RegExp");
 var prompt = require('prompt-sync')();
 var email = prompt('Enter Email Here: ');
 // UC1
-var emailRegEx =new RegExp("^[a-zA-Z0-9]{3}$");
-if(emailRegEx.test(email)) {
-    console.log("Email: "+email+" is Valid.");
+var emailRegExUc1 =new RegExp("^[a-zA-Z0-9]{3,}$");
+if(emailRegExUc1.test(email)) {
+    console.log("UC1: Email: "+email+" is Valid.");
 } else {
-    console.log("Email: "+email+" is Invalid.");
+    console.log("UC1: Email: "+email+" is Invalid.");
+}
+// UC2
+var emailRegExUc2 =new RegExp("^[a-zA-Z0-9]+@$");
+if(emailRegExUc2.test(email)) {
+    console.log("UC2: Email: "+email+" is Valid.");
+} else {
+    console.log("UC2: Email: "+email+" is Invalid.");
 }
